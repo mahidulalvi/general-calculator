cc8672d7abe7aa27f708276409a007eb59bcd65e
MD MAHIDUL HAQUE ALVI <mdmahidulhaque001@gmail.com>
2019-05-29 10:57:24 -0500

    Work done till this point

---
+++ CalculatorDeliverables/Helpers/BasicHelperMethods.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CalculatorDeliverables.Helpers
+{
+    class BasicHelperMethods
+    {
+        public MainWindow MainWindow { get; set; }
+
+        public BasicHelperMethods(MainWindow mainwindow)
+        {
+            MainWindow = mainwindow;
+        }
+
+        public bool IsInputEmpty()
+        {
+            if (MainWindow.CalcInput.Text.Any())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsInputInvalid()
+        {
+            if (MainWindow.CalcInput.Text.Contains(","))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsInputLeftUnused()
+        {
+            return true;
+        }
+
+        public bool InputHasLeadingZeroWithoutDecimals(string input)
+        {
+            if (input.StartsWith("0") && !InputHasDecimal(input))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int NumberOfLeadingZeroes(string input)
+        {
+            var result = 0;
+            bool leadingZeroOngoing = true;
+
+            for(var i = 0; i < input.Length; i++)
+            {
+                if (leadingZeroOngoing == true)
+                {
+                    if (input[i] == '0')
+                    {
+                        result += 1;
+                    }
+                    else
+                    {
+                        leadingZeroOngoing = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public bool InputHasDecimal(string input)
+        {                        
+            if(input.Contains("."))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public decimal DetermineOperatorAndCalculate(string input, decimal referenceNumber, decimal latestInput)
+        {
+            if(input == "+")
+            {
+                return referenceNumber += latestInput;
+            }
+            else if(input == "-")
+            {
+                return referenceNumber -= latestInput;
+            }
+            else if(input == "*")
+            {
+                return referenceNumber *= latestInput;
+            }
+            else if (input == "/")
+            {
+                return referenceNumber / latestInput;
+            }
+
+            return referenceNumber;
+        }
+
+        //public void ButtonOnMouseDown(object sender, MouseEventArgs e)
+        //{
+        //    Button button = e.Source as Button;
+
+        //    if(button != null)
+        //    {
+        //        button.CaptureMouse();
+        //    }
+        //}
+
+
+        //public void ButtonOnMouseUp(object sender, MouseEventArgs e)
+        //{
+        //    Button button = e.Source as Button;
+
+        //    if (button != null)
+        //    {
+        //        button.ReleaseMouseCapture();
+        //        MainWindow.CalcInput.Text += button.Content;
+        //    }
+        //}
+
+
+    }
+}
--- CalculatorDeliverables/MainWindow.xaml.cs
+++ CalculatorDeliverables/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CalculatorDeliverables.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,108 @@ namespace CalculatorDeliverables
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BasicHelperMethods BasicHelper { get; set; }
+        private List<decimal> InputNumbers { get; set; }
+        public decimal Result { get; set; }
+        public string Operator { get; set; }
+        public bool OperatorAllowed { get; set; }
+        public bool ResultShowingInCalcInput { get; set; }
+        public bool IsCalcInResetState { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            BasicHelper = new BasicHelperMethods(this);
+            InputNumbers = new List<decimal>();
+            Result = 0;
+            Operator = "";
+            OperatorAllowed = false;
+            ResultShowingInCalcInput = true;
+            IsCalcInResetState = true;
+        }        
+
+        private void BtnNums_On_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = e.Source as Button;
+            if(button != null)
+            {
+                if(button.Content.ToString() == ".")
+                {
+                    if (!BasicHelper.InputHasDecimal(CalcInput.Text) && ResultShowingInCalcInput == false)
+                    {                     
+                        CalcInput.Text += button.Content;
+                        OperatorAllowed = true;                        
+                    }
+                }    
+                else
+                {
+                    if(ResultShowingInCalcInput/* || IsCalcInResetState*/)
+                    {
+                        CalcInput.Text = "";
+                    }
+
+                    if (BasicHelper.InputHasLeadingZeroWithoutDecimals(CalcInput.Text))
+                    {
+                        var totalLeadingZeroes = BasicHelper.NumberOfLeadingZeroes(CalcInput.Text);
+
+                        var textWithoutLeadingZero = CalcInput.Text.Substring(totalLeadingZeroes);
+
+                        CalcInput.Text = "";
+                        CalcInput.Text = textWithoutLeadingZero;
+                        OperatorAllowed = true;                        
+                    }
+                    else
+                    {
+                        CalcInput.Text += button.Content;
+                        OperatorAllowed = true;                        
+                    }
+                }
+                ResultShowingInCalcInput = false;
+                IsCalcInResetState = false;
+            }
         }
+
+
+
+        private void BtnOperators_On_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = e.Source as Button;
+            if (button != null && OperatorAllowed)
+            {                
+                if (BasicHelper.InputHasLeadingZeroWithoutDecimals(CalcInput.Text))
+                {
+                    var totalLeadingZeroes = BasicHelper.NumberOfLeadingZeroes(CalcInput.Text);
+
+                    var textWithoutLeadingZero = CalcInput.Text.Substring(totalLeadingZeroes);
+                    CalcInput.Text = textWithoutLeadingZero;
+                }
+
+                var inputText = CalcInput.Text;
+                var inputTextConvertedToDecimal = Convert.ToDecimal(inputText);
+
+                if (Operator != "-")
+                {
+                    CalcDisplay.Text += inputText;
+                }
+
+                InputNumbers.Add(inputTextConvertedToDecimal);
+                CalcDisplay.Text += " " + button.Content + " ";
+
+                if (InputNumbers.Count() > 1)
+                {
+                    Result = BasicHelper.DetermineOperatorAndCalculate(Operator, Result, inputTextConvertedToDecimal);
+
+                    CalcInput.Text = $"{Result}";
+                    ResultShowingInCalcInput = true;
+                }
+                else if(InputNumbers.Count() > 0 && InputNumbers.Count() < 2)
+                {
+                    Result += InputNumbers[0];
+                    ResultShowingInCalcInput = true;
+                }
+
+                Operator = button.Content.ToString();
+            }
+        }                                     
     }
 }
